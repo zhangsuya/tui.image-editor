87fe17243372c9bd1b66f4924e8e0bd1849ac4fc
792708835@qq.com <792708835@qq.com>
2023-09-26 17:54:54 +0800

    feat：freeDraw优化显示

--- apps/image-editor/src/js/component/freeCircleSelecte.js
+++ apps/image-editor/src/js/component/freeCircleSelecte.js
@@ -58,8 +58,8 @@ class FreeCircleSelecte extends Component {
     const brush = this.getCanvas().freeDrawingBrush;
 
     setting = setting || {};
-    console.log('FreeCircleSelecte setBrush');
-    console.log(setting);
+    // console.log('FreeCircleSelecte setBrush');
+    // console.log(setting);
 
     this.width = setting.width || this.width;
     if (setting.color) {
@@ -83,137 +83,31 @@ class FreeCircleSelecte extends Component {
   }
 
   _onBeforePathCreated(path) {
-    // if (!self.renderCanvas) {
-    //   return;
-    // }
-    console.log('FreeCircleSelecte end');
-    // const canvasEl = this.getCanvas().getSelectionElement();
-    // const context = canvasEl.getContext('2d');
-    // // context.fillStyle = 'black';
-    // context.fillStyle = 'white';
-    // context.fill();
-    // this.fire(eventNames.FREE_ADDING_LINE, path);
-    // const link = document.createElement('a');
-    // link.href = canvasEl.toDataURL();
-    // const ext = 'png';
-    // link.download = `eraser_example.${ext}`;
-    // link.click();
-    // console.log('FreeCircleSelecte click');
-
-    const newCanvasEle = fabric.util.createCanvasElement();
-    const myCanvas = this.getCanvas().getSelectionElement();
-
-    const newCnavs = new fabric.Canvas(newCanvasEle);
-    newCanvasEle.width = myCanvas.width;
-    newCanvasEle.height = myCanvas.height;
-    newCanvasEle.style.backgroundColor = 'black';
-    const newCanvasContext = newCanvasEle.getContext('2d');
-    newCanvasContext.fillStyle = 'black';
-    newCanvasContext.fillRect(0, 0, newCanvasEle.width, newCanvasEle.height);
-
-    // const secondCanvas = document.createElement('canvas');
-    // const cctx = secondCanvas.getContext('2d');
-    // secondCanvas.width = myCanvas.width;
-    // secondCanvas.height = myCanvas.height;
-
-    newCanvasContext.fillStyle = 'white';
-    newCanvasContext.fill();
-    const realPath = path.path;
-    realPath.stroke = 'black';
-    // realPath.strokeWidth = 2;
-    // realPath.strokeDashArray = [5, 5];
-    newCnavs.add(realPath);
-    // eslint-disable-next-line no-undef
-    // this._animateDashedLine(realPath);
-    newCnavs.requestRenderAll();
-    const link = document.createElement('a');
-    link.href = newCanvasEle.toDataURL();
-    const ext = 'png';
-    link.download = `eraser_example.${ext}`;
-    link.click();
-    // realPath.setCoords();
-    // realPath.animate('strokeDashOffset', '-=3', {
-    //   duration: 100,
-    //   onchange: this.getCanvas().renderAll.bind(this.getCanvas()),
-    //   repeat: true,
-    // });
+    // console.log('FreeCircleSelecte end');
+    console.log(path);
   }
 
   _onPathCreated(path) {
-    console.log(path.path);
-    // strokeDashArray: [5, 5]
-
-    // if (!self.renderCanvas) {
-    //   return;
-    // }
     console.log('FreeCircleSelecte end');
     path.path.fill = 'white';
     this.getCanvas().renderTop();
     const canvasEl = this.getCanvas().getSelectionElement();
 
+    const context = canvasEl.getContext('2d');
+    // context.fillStyle = 'black';
+    context.fillStyle = 'white';
+    context.fill();
     /* 先复制出来一份 */
     const newCanvasEle = document.createElement('canvas');
     newCanvasEle.width = canvasEl.width;
     newCanvasEle.height = canvasEl.height;
     const newCanvasContext = newCanvasEle.getContext('2d');
+    newCanvasContext.fillStyle = 'black';
+    newCanvasContext.fillRect(0, 0, canvasEl.width, canvasEl.height); // redraw the saved chart back to the main canvas
     newCanvasContext.drawImage(canvasEl, 0, 0);
-    /* 填充白色  这里可能也需要一个canvas中转下*/
 
-    // newCanvasContext.fillStyle = 'white';
-    // newCanvasContext.fill();
-    // newCanvasContext.drawImage(firstCanvas, 0, 0);
-
-    // const secondCanvas = document.createElement('canvas');
-    // const cctx = secondCanvas.getContext('2d');
-    // secondCanvas.width = canvasEl.width;
-    // secondCanvas.height = canvasEl.height;
-    // cctx.drawImage(canvasEl, 0, 0);
-
-    // newCanvasContext.fillStyle = 'black';
-    // newCanvasContext.fillRect(0, 0, canvasEl.width, canvasEl.height);
-    // // redraw the saved chart back to the main canvas
-    // newCanvasContext.drawImage(secondCanvas, 0, 0);
-    // 真正的填充
-
-    const context = canvasEl.getContext('2d');
-    // context.fillStyle = 'black';
-    context.fillStyle = 'white';
-    context.fill();
-    const secondCanvas = document.createElement('canvas');
-    const cctx = secondCanvas.getContext('2d');
-    secondCanvas.width = canvasEl.width;
-    secondCanvas.height = canvasEl.height;
-    cctx.drawImage(canvasEl, 0, 0);
-
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, canvasEl.width, canvasEl.height);
-    // redraw the saved chart back to the main canvas
-    context.drawImage(secondCanvas, 0, 0);
-
-    // 把填充后的canvasEl复制出来
-    const firstCanvas = document.createElement('canvas');
-    const firstcctx = firstCanvas.getContext('2d');
-    firstCanvas.width = canvasEl.width;
-    firstCanvas.height = canvasEl.height;
-    firstcctx.drawImage(canvasEl, 0, 0);
-    // 把原来的canvasEl复制出来
-    // context.globalAlpha = 0;
-    // context.fillStyle = 'rgba(255, 255, 255, 0)';
-    // context.fillRect(0, 0, canvasEl.width, canvasEl.height);
-    // context.drawImage(newCanvasEle, 0, 0);
-
-    const params = this.graphics.createObjectProperties(path.path);
-    this.fire(eventNames.FREE_ADDING_LINE, params);
-    // const link = document.createElement('a');
-    // link.href = firstCanvas.toDataURL();
-    // const ext = 'png';
-    // link.download = `eraser_example.${ext}`;
-    // link.click();
-
-    // const link2 = document.createElement('a');
-    // link2.href = newCanvasEle.toDataURL();
-    // link2.download = `eraser_example.${ext}`;
-    // link2.click();
+    // const params = this.graphics.createObjectProperties(path.path);
+    this.fire(eventNames.FREE_ADDING_LINE, newCanvasEle.toDataURL());
   }
 
   // 定义动画函数
